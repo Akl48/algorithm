8fd76af34709088067cb64cb21c9fdb93f6c7212
Zzzz_TR <z1057075812@outlook.com>
2023-09-17 16:02:32 +0800

    [update]打家劫舍2 & 回文数

--- LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode.cpp
@@ -33,8 +33,51 @@ public:
         }
         return dp[size-1];
     }
+
+    int rob(vector<int>& nums, int start, int end) {
+        int first = nums[start], second = max(nums[start], nums[start+1]);
+        for (int i = start + 2; i <= end; ++i) {
+            int tmp = second;
+            second = max(first + nums[i], second);
+            first = tmp;
+        }
+        return second;
+    }
+
+    int rob_v2(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int size = nums.size();
+        if (size <= 3) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        return max(rob(nums, 0, size - 2), rob(nums, 1, size - 1));
+    }
+
+    string reverseString(const string& str) {
+        string reversedStr;
+        int length = str.length();
+
+        for (int i = length - 1; i >= 0; --i) {
+            reversedStr += str[i];
+        }   
+
+        return reversedStr;
+    }
+
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        string str = s.to_string();
+        string newStr = reverseString(str);
+        if (str == newStr) {
+            return true;
+        }
+        return false;
+    }
 };
 
 int main() {
-    vector<int> msg = {114,117,207,117,235,82,90,67,143,146,53,108,2005,169,241,202,144,240};
+
 }
